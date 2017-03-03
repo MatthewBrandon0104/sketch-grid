2679d00016a44b5c555c35185ebbb97c85178128
Matthew Brandon <matthewbrandon0104@gmail.com>
2017-03-03 01:06:19 -0800

    sketch grid functioning, need to add styling

---
+++ script/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+var reply = 4;
+var boxDim = function () {
+								z = 100 / reply +'%';
+                return z;
+}
+
+var addBox = function() {
+								$('.wrapper').append('<div class="box"></div>');
+}
+
+var changeDim = function () {
+								$('.box').css({width: boxDim, height: boxDim});
+}
+
+var loop = function () {
+	for (var i = 0; i < reply * reply; i++) {
+		addBox();
+	}
+}
+
+loop();
+
+$('.box').on('mouseenter', function() {
+	$(this).css('background-color', 'beige');
+});
+
+$('.button').click(function() {
+	$('.box').remove();
+  reply = prompt('How many boxes would you like?');
+  loop();
+  changeDim();
+  $('.box').on('mouseenter', function() {
+	$(this).css('background-color', 'beige');
+
+});
+});
+});
